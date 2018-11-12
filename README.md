f7dbdb26af25f9e2090bf5762ec4ee87901cfb10
fedeg <fedeg@Hp-Fede.lan>
2018-11-12 15:13:10 +0100

    soluzione-aula

---
+++ README.md
@@ -0,0 +1,20 @@
+Scrivere un'applicazione per "espandere" una schedina Totocalcio.
+
+Data una schedina in formato "compatto", stampare l'elenco completo delle colonne corrispondenti.
+
+Ad esempio, data la schedina seguente:
+
+```
+1
+1X
+X2
+```
+
+si stamperanno le seguenti colonne:
+
+```
+1  1  1  1
+1  X  1  X
+X  X  2  2
+```
+ 
---
+++ src/it/polito/tdp/toto/Schedina.java
@@ -0,0 +1,49 @@
+package it.polito.tdp.toto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedina {
+
+	private int N;
+
+	private List<Risultato> colonna;
+
+	public Schedina(int N) {
+		this.N = N;
+		this.colonna = new ArrayList<Risultato>();
+	}
+	
+	// clone constructor
+	public Schedina(Schedina other) {
+		this.N = other.N ;
+		this.colonna = new ArrayList<>(other.colonna) ;
+	}
+
+	public void add(Risultato valore) {
+		if (colonna.size() < N) {
+			colonna.add(valore);
+		} else {
+			throw new IllegalStateException("Too many elements in Schedina") ;
+		}
+	}
+
+	public Risultato get(int partita) {
+		return colonna.get(partita);
+	}
+	
+	public void removeLast() {
+		colonna.remove(colonna.size()-1) ;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Risultato r : colonna) {
+			sb.append(r.toString());
+			sb.append("-") ;
+		}
+		sb.deleteCharAt(sb.length()-1) ;
+		return sb.toString();
+	}
+
+}
